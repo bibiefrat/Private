ed2da688228dc8d1dae1c14f49e53babf66a1bcf
bibi <bibi.efrat@gmail.com>
2023-01-05 18:08:07 +0200

    commit all

--- temp4.py
+++ temp4.py
@@ -1,30 +1,18 @@
-def is_palindrome(num):
-    # Skip single-digit inputs
-    if num // 10 == 0:
-        return False
-    temp = num
-    reversed_num = 0
+def coro():
+    print ('before yield')
+    a = yield 'the yield value'
+    b = yield a
+    print(f"done!")
+c=coro()
+    # this does not execute the generator, only creates it
 
-    while temp != 0:
-        reversed_num = (reversed_num * 10) + (temp % 10)
-        temp = temp // 10
+     # If you use c.send('a value') here it could _not_ do anything with the value
+     # so it raises an TypeError! Remember, the generator was not executed yet,
+     # only created, it is like the execution is before the `print 'before yield'`
 
-    if num == reversed_num:
-        return num
-    else:
-        return False
+     # This line could be `c.send(None)` too, the `None` needs to be explicit with
+     # the first use of `send()` to show that you know it is the first iteration
+print (next(c)) # will print 'before yield' then 'the yield value' that was yield
 
-
-
-def infinite_sequence():
-    num = 0
-    while True:
-        yield num
-        num += 1
-
-
-
-for i in infinite_sequence():
-    pal = is_palindrome(i)
-    if pal:
-        print(i)
+print (c.send('first value sent')) # will print 'first value sent'
+print (c.send('sec value sent')) # will print 'first value sent'
